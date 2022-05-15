3c1893bfeb37d024c46a7508e55349f006c69cd4
N1r-P <pr156292834552021@163.com>
2022-05-15 17:44:04 +0800

    project

---
+++ Project/src/base.java
@@ -0,0 +1,8 @@
+public class base {
+    private int a = 10;
+    private int b = 2;
+    public void add()
+    {
+        System.out.println(a+b);
+    }
+}
---
+++ Project/src/run.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Base64;
+
+public class run {
+    private base p = new base();
+    private static run r = new run();
+
+    public static void main(String[] args) throws IOException {
+        r.p.add();
+    }
+}

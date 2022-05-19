542ac95c00af781714b406b1a9f86ccd86c1c776
N1r-P <pr156292834552021@163.com>
2022-05-19 16:23:43 +0800

    Revert "change"
    This reverts commit 9e435d5db43e9a0359846c598d7f1e983872d0ef.

---
+++ Project/src/base.java
@@ -0,0 +1,9 @@
+public class base {
+    private int a = 10;
+    private int b = 2;
+    public void add()
+    {
+        System.out.println("the result is:");
+        System.out.println(a+b);
+    }
+}

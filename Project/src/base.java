7801de65352b81ab468641423f3827dd1d79bfe4
N1r-P <pr156292834552021@163.com>
2022-05-19 21:04:44 +0800

    Revert "Revert "change""
    This reverts commit 542ac95c00af781714b406b1a9f86ccd86c1c776.

--- Project/src/base.java
+++
@@ -1,9 +0,0 @@
-public class base {
-    private int a = 10;
-    private int b = 2;
-    public void add()
-    {
-        System.out.println("the result is:");
-        System.out.println(a+b);
-    }
-}
---
+++ Project/src/user/user_information.java
@@ -0,0 +1,59 @@
+package user;
+
+public class user_information {
+    private String name;
+    private String password;
+    private String phone_number;
+    private String address;
+
+    /**
+     * initialize the property
+     * @param name
+     * @param password
+     * @param phone_number
+     * @param address
+     */
+    public user_information(String name,String password,String phone_number,String address)
+    {
+        this.name = name;
+        this.password = password;
+        this.phone_number = phone_number;
+        this.address = address;
+    }
+
+    /**
+     * get the name of user
+     * @return name
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /**
+     * get the password of user
+     * @return password
+     */
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    /**
+     * get the phone_number of user
+     * @return phone_number
+     */
+    public String getPhone_number()
+    {
+        return this.phone_number;
+    }
+
+    /**
+     * get the address of user
+     * @return address
+     */
+    public String getAddress()
+    {
+        return this.address;
+    }
+}

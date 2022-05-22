4c79251ab3f092dc07ec701a5ac64fee380470b5
N1r-P <pr156292834552021@163.com>
2022-05-22 17:37:44 +0800

    some small change

--- Project/src/products/Product.java
+++ Project/src/products/Product.java
@@ -8,7 +8,7 @@ public class Product {
     /**
      *
      */
-    public Product()
+    public Product(String name,String description,double price)
     {
         this.name=name;
         this.description=description;
